3b4d9a780a01ca248ce067568cf1e5258a1968d5
developerVivekBharadwaj <devvivekbharadwaj@gmail.com>
2024-07-06 18:00:44 +0530

    City Data store and Model Class

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
 namespace CityInfo.API.Controllers
@@ -10,11 +11,13 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>
-            {
-                new{ id = 1, Name = "New York"},
-                new{ id = 2, Name = "Antwerp"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCities(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
