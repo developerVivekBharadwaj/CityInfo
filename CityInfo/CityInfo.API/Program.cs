118b65ec7f57e01e3e721a3ce0828f71f2f20d8b
developerVivekBharadwaj <devvivekbharadwaj@gmail.com>
2024-07-07 17:34:52 +0530

    IOC, logging , Exception handling, serilog

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
 // Add services to the container.
 
 builder.Services.AddControllers(options =>
     options.ReturnHttpNotAcceptable = true
 ).AddNewtonsoftJson()
  .AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddProblemDetails();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +25,10 @@ builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
