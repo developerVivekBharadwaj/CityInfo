97bb48f8cefc3205dd6f9b120201ac58b1486309
developerVivekBharadwaj <devvivekbharadwaj@gmail.com>
2024-07-28 17:30:12 +0530

    Repository Pattern implementation

---
+++ CityInfo/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestsAsync(int cityId);
+
+        Task<PointOfInterest> GetPointOfInterestsAsync(int cityId,
+            int pointOfInterestId);
+
+    }
+}
