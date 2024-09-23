9b8a8851e33dd882b2ca7c9808d5cec7ac65435a
developerVivekBharadwaj <devvivekbharadwaj@gmail.com>
2024-09-23 06:33:13 +0530

    Versioning
    Route

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Asp.Versioning;
+using AutoMapper;
 using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -9,7 +10,8 @@ namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Authorize(Policy = "MustBeFromAntwerp")]
-    [Route("api/cities/{cityId}/pointsofinterest")]
+    [Route("api/v{version:apiVersion}/cities/{cityId}/pointsofinterest")]
+    [ApiVersion(2)]
     public class PointsOfInterestController : ControllerBase
     {
         private readonly ILogger<PointsOfInterestController> _logger;
